6ed5d830767e868cef64ea52933036fcc0139827
amine001 <aminefathi@tutanota.com>
2023-02-13 21:31:02 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Dashbord from "./scenes/dashboard"
+import Sidebar from "./scenes/global/Sidebar"
 import Topbar from "./scenes/global/Topbar"
+import Team from "./scenes/team"
+import Invoices from "./scenes/invoices"
+import Contacts from "./scenes/contacts"
+// import Bar from "./scenes/bar"
+import Form from "./scenes/form"
+// import Line from "./scenes/line"
+// import Pie from "./scenes/pie"
+// import FAQ from "./scenes/faq"
+// import Geography from "./scenes/geography"
+// import Calendar from "./scenes/calendar" 
 
 
 function App() {
@@ -12,8 +25,22 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route exact path="/" element={<Dashbord />} />
+              <Route exact path="/team" element={<Team />} />
+              <Route exact path="/contacts" element={<Contacts />} />
+              <Route exact path="/Invoices" element={<Invoices />} />
+              {/* <Route exact path="/bar" element={<Bar />} /> */}
+              <Route exact path="/form" element={<Form />} />
+              {/* <Route exact path="/line" element={<Line />} /> */}
+              {/* <Route exact path="/pie" element={<Pie />} /> */}
+              {/* <Route exact path="/faq" element={<FAQ />} /> */}
+              {/*    <Route exact path="/geography" element={<Geography />} /> */}
+              {/*      <Route exact path="/calendar" element={<Calendar />} /> */}
+            </Routes>
           </main>
 
         </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { Typography, Box, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+
+const Header = ({ title, subtitle }) => {
+    // setup the theme and the color palette mode
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode)
+
+    return (
+        <Box mb="30px">
+            <Typography
+                variant="h2"
+                color={colors.grey[100]}
+                fontWeight="bold"
+                sx={{ mb: "5px" }}
+            >
+                {title}
+            </Typography>
+            <Typography
+                variant="h5"
+                color={colors.greenAccent[400]}
+            >
+                {subtitle}
+            </Typography>
+        </Box>
+    )
+}
+
+export default Header
